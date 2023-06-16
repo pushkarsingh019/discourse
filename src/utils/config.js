040fa11646466a013f14090aeb38e2715b65c7fd
Pushkar Singh <stoazest@gmail.com>
2023-06-16 14:18:15 +0530

    ADDED ENV VARIABLES

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,1 @@
-// export const backendUrl = "https://forum-backend-production-bc14.up.railway.app";
-export const backendUrl = "http://localhost:8080"
+export const backendUrl = process.env.REACT_APP_BACKENDURL || "http://localhost:8080";
