b5e8a3d2127a451ef64428055b715d7952167116
Pushkar Singh <stoazest@gmail.com>
2023-06-26 09:58:24 +0530

    ADD : Explore Screen

--- src/screens/ExploreScreen.jsx
+++ src/screens/ExploreScreen.jsx
@@ -1,31 +1,62 @@
-import { useState } from "react";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
-import Modal from "../components/Modal";
+import { useContext } from "react";
+import { storeContext } from "../utils/store";
+import { useEffect } from "react";
+import Post from "../components/Post";
+import FloatingCreateButton from "../components/FloatingCreateButton";
+import MobileTopBar from "../components/MobileTopBar";
 
 const ExploreScreen = () => {
-    const [showModal, setShowModal] = useState(false);
-    const handleToggleState = (toggleState) => setShowModal(toggleState);
+    const { posts, postReducer } = useContext(storeContext);
+
+    useEffect(() => {
+        postReducer({
+            type: "fetch_posts",
+        });
+        // eslint-disable-next-line
+    }, []);
+
     return (
-        <section className="layout">
+        <section className="layout bg-gray-100">
             <Header />
             <Menu />
-            <main className="main-content">
-                <h3>the explore page</h3>
-                <button onClick={() => setShowModal(true)}>open modal</button>
-                <Modal isOpen={showModal} toggleIsOpen={handleToggleState}>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Nostrum nesciunt ipsam beatae facere, eaque dolor ipsum
-                        eligendi illo voluptas ducimus. Necessitatibus odit nam
-                        minus odio temporibus numquam adipisci ut, dignissimos,
-                        error dolores labore magni quae debitis saepe asperiores
-                        est. Enim vel, sequi consectetur beatae consequatur
-                        voluptatibus temporibus ex provident et.
-                    </p>
-                    <button onClick={() => setShowModal(false)}>cancel</button>
-                </Modal>
+            <FloatingCreateButton />
+            <MobileTopBar text={`Explore`} />
+            <main className="main-content px-3 pt-2">
+                <br />
+                <h2 className="text-2xl font-sans font-medium mb-3 hidden md:block">
+                    Explore
+                </h2>
+                {posts.length === 0 ? (
+                    <p className="text-xl">no posts yet 😔</p>
+                ) : (
+                    posts.map((post) => {
+                        return (
+                            <Post
+                                id={post._id}
+                                key={post._id}
+                                post={post.post}
+                                username={post.authorDetails.username}
+                                time={post.time}
+                                likes={post.likes}
+                                community={post.community}
+                                authorDetails={post.authorDetails}
+                                comments={post.comments}
+                            />
+                        );
+                    })
+                )}
             </main>
+            <div className="aside-content">
+                <h3 className="text-2xl">Who to follow?</h3>
+                <ul>
+                    <li>Pushkar Singh</li>
+                    <li>Pushkar Singh</li>
+                    <li>Pushkar Singh</li>
+                    <li>Pushkar Singh</li>
+                </ul>
+            </div>
         </section>
     );
 };
